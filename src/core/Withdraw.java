90b2da7d0dd3713cdd12c80d66c1d0c980a35e94
aazar412 <aazar412@gmail.com>
2023-01-05 17:42:04 -0500

    updated withdraw page

--- src/core/Withdraw.java
+++ src/core/Withdraw.java
@@ -3,5 +3,7 @@ package core;
 public class Withdraw {
     public static void main(String[] args) {
         System.out.println("Withdrawing");
+
+        System.out.println("what to withdraw");
     }
 }
